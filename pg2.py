b2b87c712e89ee88831afaee892aca3139bf9380
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-07-01 06:41:50 +0600

    Python code

---
+++ pg2.py
@@ -0,0 +1,6 @@
+import pyttsx3
+engine = pyttsx3.init()
+# engine.say("can you speak like this")
+engine.say(''' can you say my name 
+           my name is shadman and i am good an english and try my best to bit me''')
+engine.runAndWait()
